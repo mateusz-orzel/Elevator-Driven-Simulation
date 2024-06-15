8268bea6a41592f791f71393e3f902e42df73ec9
Mateusz <mateuszorzel44@gmail.com>
2024-06-15 03:20:05 +0200

    basic menu and adding simulation parameters

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import heapq
 import time
 import threading
 import os
+import sys
+import time
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -192,6 +194,10 @@ class Elevator:
         if self.floor_queue:
             _, dest = heapq.heappop(self.floor_queue)
             self.go_floor(dest)
+
+    def make_awaria(self):
+        if rd.random() < self.emergency_rate:
+            print()
     
         
     def draw(self, window):
@@ -210,23 +216,150 @@ class Elevator:
             self.move()
             time.sleep(1)
 
+
+class Menu:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.font = pygame.font.Font(None, 32)
+        
+        self.start_button_width = 200
+        self.start_button_height = 50
+        self.start_button_x = (WINDOW_WIDTH // 2) - (self.start_button_width // 2)
+        self.start_button_y = (WINDOW_HEIGHT // 2) + 250
+        self.start_button = pygame.Rect(self.start_button_x, self.start_button_y, self.start_button_width, self.start_button_height)
+        self.start_button_text = self.font.render('Start Simulation', True, WHITE)
+
+        self.slider_rect_width = 200
+        self.slider_rect_x = (WINDOW_WIDTH // 2) - (self.slider_rect_width // 2)
+        self.slider_rect_y = (WINDOW_HEIGHT // 2) - 50
+        self.slider_rect = pygame.Rect(self.slider_rect_x, self.slider_rect_y, self.slider_rect_width, 10)
+        self.slider_handle_pos = self.slider_rect_x
+        self.slider_dragging = False
+        self.emergency_rate = 0.0
+
+        self.freq_slider_rect_x = (WINDOW_WIDTH // 2) - (self.slider_rect_width // 2)
+        self.freq_slider_rect_y = (WINDOW_HEIGHT // 2) + 50
+        self.freq_slider_rect = pygame.Rect(self.freq_slider_rect_x , self.freq_slider_rect_y, self.slider_rect_width, 10)
+        self.freq_slider_handle_pos = self.freq_slider_rect_x 
+        self.freq_slider_dragging = False
+        self.people_generation_freq = 1.0 
+
+        self.checkbox_rect = pygame.Rect(WINDOW_WIDTH // 2 - 100, (WINDOW_HEIGHT // 2) + 150, 20, 20)
+        self.checkbox_checked = False
+
+    def main(self):
+        self.draw()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.start_button.collidepoint(event.pos):
+                        self.start_new_simulation()
+                    elif self.slider_rect.collidepoint(event.pos) or abs(event.pos[0] - self.slider_handle_pos) <= 5:
+                        if not self.checkbox_checked:
+                            self.slider_dragging = True
+                    elif self.freq_slider_rect.collidepoint(event.pos) or abs(event.pos[0] - self.freq_slider_handle_pos) <= 5:
+                        if not self.checkbox_checked:
+                            self.freq_slider_dragging = True
+                    elif self.checkbox_rect.collidepoint(event.pos):
+                        self.checkbox_checked = not self.checkbox_checked
+
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.slider_dragging = False
+                    self.freq_slider_dragging = False
+
+                elif event.type == pygame.MOUSEMOTION:
+                    if self.slider_dragging:
+                        self.slider_handle_pos = max(self.slider_rect_x, min(event.pos[0], self.slider_rect_x + self.slider_rect_width))
+                        self.emergency_rate = (self.slider_handle_pos - self.slider_rect_x) / self.slider_rect_width
+                    elif self.freq_slider_dragging:
+                        self.freq_slider_handle_pos = max(self.freq_slider_rect_x, min(event.pos[0], self.freq_slider_rect_x + self.slider_rect_width))
+                        self.people_generation_freq = int(1 + (self.freq_slider_handle_pos - self.freq_slider_rect_x) / self.slider_rect_width * 98)
+
+            self.draw()
+            pygame.display.update()
+
+    def start_new_simulation(self):
+        simulation = Simulation(window=self.window, total_floors=6, emergency_rate=self.emergency_rate, people_generation_freq=self.people_generation_freq, manual_mode=self.checkbox_checked)
+        simulation.main()
+
+    def draw(self):
+        self.window.fill(WHITE)
+
+        text = self.font.render('Parameters', True, DARK_GRAY)
+        text_rect = text.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 - 100))
+        self.window.blit(text, text_rect)
+
+        pygame.draw.rect(self.window, DARK_GRAY, self.start_button)
+        start_button_text_rect = self.start_button_text.get_rect(center=self.start_button.center)
+        self.window.blit(self.start_button_text, start_button_text_rect)
+
+        pygame.draw.rect(self.window, BLACK, self.slider_rect)
+        pygame.draw.circle(self.window, BLACK, (self.slider_handle_pos, self.slider_rect_y + 5), 7)
+        rate_text = self.font.render(f'Emergency Rate: {self.emergency_rate:.3f}', True, DARK_GRAY)
+        rate_text_rect = rate_text.get_rect(center=(WINDOW_WIDTH // 2, self.slider_rect_y + 30))
+        self.window.blit(rate_text, rate_text_rect)
+
+        slider_color = DARK_GRAY if self.checkbox_checked else BLACK
+        pygame.draw.rect(self.window, slider_color, self.freq_slider_rect)
+        pygame.draw.circle(self.window, slider_color, (self.freq_slider_handle_pos, self.freq_slider_rect_y + 5), 7)
+        freq_text = self.font.render(f'People Generation Freq: {self.people_generation_freq:.1f}', True, slider_color)
+        freq_text_rect = freq_text.get_rect(center=(WINDOW_WIDTH // 2, self.freq_slider_rect_y + 30))
+        self.window.blit(freq_text, freq_text_rect)
+
+        pygame.draw.rect(self.window, BLACK, self.checkbox_rect, 2)
+        if self.checkbox_checked:
+            pygame.draw.line(self.window, BLACK, (self.checkbox_rect.left + 4, self.checkbox_rect.centery), (self.checkbox_rect.centerx, self.checkbox_rect.bottom - 4), 2)
+            pygame.draw.line(self.window, BLACK, (self.checkbox_rect.centerx, self.checkbox_rect.bottom - 4), (self.checkbox_rect.right - 4, self.checkbox_rect.top + 4), 2)
+
+        checkbox_label = self.font.render('Manual Adding People', True, DARK_GRAY)
+        checkbox_label_rect = checkbox_label.get_rect(midleft=(self.checkbox_rect.right + 10, self.checkbox_rect.centery))
+        self.window.blit(checkbox_label, checkbox_label_rect)
+
+        pygame.display.flip()
+
+
 class Simulation:
 
-    def __init__(self, total_floors = 4):
-        self.window_width = WINDOW_WIDTH
-        self.window_height = WINDOW_HEIGHT
+    def __init__(self, window, emergency_rate, people_generation_freq, manual_mode, total_floors = 4):
+
+
+        self.total_floors = total_floors
+        self.emergency_rate = emergency_rate
+        self.people_generation_freq = people_generation_freq
+        self.manual_mode = manual_mode
+
+
         self.run = True
-        self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        self.window = window
         self.total_floors = total_floors
         self.floors = {i: [] for i in range(self.total_floors)}
-        self.buttons = [pygame.Rect(self.window_width - 100, WINDOW_HEIGHT - int(0.5*(self.window_height//self.total_floors)) - 20 - (i * (self.window_height // self.total_floors)), 40, 40) for i in range(self.total_floors)]
+        self.buttons = [pygame.Rect(WINDOW_WIDTH - 100, WINDOW_HEIGHT - int(0.5*(WINDOW_HEIGHT//self.total_floors)) - 20 - (i * (WINDOW_HEIGHT // self.total_floors)), 40, 40) for i in range(self.total_floors)]
 
-        self.elevator = Elevator(self.window_width - 700, 500, 100, WINDOW_HEIGHT//self.total_floors, self.total_floors)
+        self.elevator = Elevator(WINDOW_WIDTH - 700, 500, 100, WINDOW_HEIGHT//self.total_floors, self.total_floors)
 
         self.elevator_thread = threading.Thread(target=self.elevator.run)
         self.elevator_thread.daemon = True
-        self.elevator_thread.start()
+        self.elevator_thread.start()\
+        
 
+        self.last_people_generation_time = time.time()
+
+    def generate_people(self):
+
+        time_now = time.time()
+
+        if time_now - self.last_people_generation_time > (60/self.people_generation_freq):
+
+            floor = rd.randint(0, self.total_floors - 1)
+            self.create_people(floor)
+
+            self.last_people_generation_time = time_now
+                
 
     def create_people(self, floor):
         
@@ -239,37 +372,40 @@ class Simulation:
             direction_floor = rd.randint(0, self.total_floors - 1)
 
         try:
-            person = Person(floor, direction_floor, self.window_width - 150, WINDOW_HEIGHT - self.window_height//self.total_floors - ((floor - 1) * (self.window_height // self.elevator.total_floors)) - people_height, people_width, people_height, person_before=self.floors[floor][-1])
+            person = Person(floor, direction_floor, WINDOW_WIDTH - 150, WINDOW_HEIGHT - WINDOW_HEIGHT//self.total_floors - ((floor - 1) * (WINDOW_HEIGHT // self.elevator.total_floors)) - people_height, people_width, people_height, person_before=self.floors[floor][-1])
         
         except:
-            person = Person(floor, direction_floor, self.window_width - 150, WINDOW_HEIGHT - self.window_height//self.total_floors - ((floor - 1) * (self.window_height // self.elevator.total_floors)) - people_height, people_width, people_height)
+            person = Person(floor, direction_floor, WINDOW_WIDTH - 150, WINDOW_HEIGHT - WINDOW_HEIGHT//self.total_floors - ((floor - 1) * (WINDOW_HEIGHT // self.elevator.total_floors)) - people_height, people_width, people_height)
         
         self.floors[floor].append(person)
 
 
-    def draw_button(self):
-        for i, btn in enumerate(self.buttons):
-            pygame.draw.rect(self.window, GRAY, btn) 
+    def draw_button(self, mode):
 
-            font = pygame.font.Font(None, 65)
-            text = font.render(f'+', True, BLACK)
-            text_rect = text.get_rect(center=(btn.centerx, btn.centery))
-            text_rect.y -= 3
-            
-            self.window.blit(text, text_rect)
+        if mode:
+            for i, btn in enumerate(self.buttons):
+                pygame.draw.rect(self.window, GRAY, btn) 
+
+                font = pygame.font.Font(None, 65)
+                text = font.render(f'+', True, BLACK)
+                text_rect = text.get_rect(center=(btn.centerx, btn.centery))
+                text_rect.y -= 3
+                
+                self.window.blit(text, text_rect)
 
 
     def draw_floors(self):
-        floor_height = self.window_height // self.total_floors
+        floor_height = WINDOW_HEIGHT // self.total_floors
 
         for i in range(self.total_floors):
-            y = self.window_height - (i + 1) * floor_height
+            y = WINDOW_HEIGHT - (i + 1) * floor_height
 
-            pygame.draw.line(self.window, BLACK, (0, y), (self.window_width, y), 2)
+            pygame.draw.line(self.window, BLACK, (0, y), (WINDOW_WIDTH, y), 2)
 
             font = pygame.font.Font(None, 36)
             text = font.render(f'Floor {i}', True, BLACK)
             self.window.blit(text, (10, y + 5))
+
 
     def main(self):
                             
@@ -277,9 +413,10 @@ class Simulation:
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.run = False
+                    pygame.quit()
+                    sys.exit()
 
-                elif event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN and self.manual_mode:
 
                     pos = pygame.mouse.get_pos()
                     for i, btn in enumerate(self.buttons):
@@ -288,9 +425,11 @@ class Simulation:
 
             self.window.fill(WHITE)
 
+            if not self.manual_mode:
+                self.generate_people()
+
             self.draw_floors()
-            self.draw_button()
-            
+            self.draw_button(self.manual_mode)
 
             self.elevator.draw(self.window)
 
@@ -309,5 +448,5 @@ class Simulation:
 
 
 if __name__ == "__main__":
-    simulation = Simulation(total_floors=6)
-    simulation.main()
+    menu = Menu()
+    menu.main()
